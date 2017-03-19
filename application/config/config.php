d496f59e6a31131a5a9052a0fb2d651bb901fc43
JakobWångö <jabbeboy@gmail.com>
2017-03-19 22:25:35 +0100

    change..

--- application/config/config.php
+++ application/config/config.php
@@ -1,46 +1,11 @@
 <?php
 
 /**
- * Configuration
- *
- * For more info about constants please @see http://php.net/manual/en/function.define.php
+ * Configuration file
  */
 
-/**
- * Configuration for: Error reporting
- * Useful to show every little problem during development, but only show hard errors in production
- */
-define('ENVIRONMENT', 'dev');
-
-if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-}
-
-/**
- * Configuration for: URL
- * Here we auto-detect your applications URL and the potential sub-folder. Works perfectly on most servers and in local
- * development environments (like WAMP, MAMP, etc.). Don't touch this unless you know what you do.
- *
- * URL_PUBLIC_FOLDER:
- * The folder that is visible to public, users will only have access to that folder so nobody can have a look into
- * "/application" or other folder inside your application or call any other .php file than index.php inside "/public".
- *
- * URL_PROTOCOL:
- * The protocol. Don't change unless you know exactly what you do. This defines the protocol part of the URL, in older
- * versions of MINI it was 'http://' for normal HTTP and 'https://' if you have a HTTPS site for sure. Now the
- * protocol-independent '//' is used, which auto-recognized the protocol.
- *
- * URL_DOMAIN:
- * The domain. Don't change unless you know exactly what you do.
- *
- * URL_SUB_FOLDER:
- * The sub-folder. Leave it like it is, even if you don't use a sub-folder (then this will be just "/").
- *
- * URL:
- * The final, auto-detected URL (build via the segments above). If you don't want to use auto-detection,
- * then replace this line with full URL (and sub-folder) and a trailing slash.
- */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 define('URL_PUBLIC_FOLDER', 'public');
 define('URL_PROTOCOL', '//');
@@ -50,12 +15,11 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
 
 /**
  * Configuration for: Database
- * This is the place where you define your database credentials, database type etc.
  */
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_NAME', 'template');
+define('DB_NAME', 'logger');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_CHARSET', 'utf8');
--- application/core/controller.php
+++ application/core/controller.php
@@ -26,14 +26,7 @@ class Controller
      */
     private function openDatabaseConnection()
     {
-        // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
-        // "objects", which means all results will be objects, like this: $result->user_name !
-        // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
         $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
-
-        // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
         $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);
     }
 
@@ -44,6 +37,7 @@ class Controller
     public function loadModel()
     {
         require APP . 'model/model.php';
+
         // create new "model" (and pass the database connection)
         $this->model = new Model($this->db);
     }
